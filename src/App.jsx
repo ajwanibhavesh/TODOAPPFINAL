2a7601015ce864662ce71542c0f84c957cdc1745
ajwanibhavesh <145528658+ajwanibhavesh@users.noreply.github.com>
2024-02-21 19:42:33 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react'
+import "./App.css"
+import Todoinput from './assets/Todoinput'
+import TodoList from './assets/TodoList';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const[listTodo,setListTodo] = useState([]);
+  let addList = (inputText) =>{
+    if(inputText!=='')
+    setListTodo([...listTodo,inputText]);
+  }
+  const deleteListItem = (key)=>{
+    let newListTodo = [...listTodo];
+    newListTodo.splice(key,1)
+    setListTodo([...newListTodo])
+  }
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+     <div className="main-container">
+       <div className="center-container">
+        <Todoinput addList={addList} />
+        <h1 className="app-heading">TODO</h1>
+        <hr/>
+        {listTodo.map((listItem,i)=>{
+          return (
+            <TodoList index = {i} key={i} item={listItem} deleteItem = {deleteListItem}/>
+          )
+        })}
+        
+
+       </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
   )
 }
 
